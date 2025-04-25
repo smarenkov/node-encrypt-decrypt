8f6c41667117e9cdcf8ba791c5733d5cf46813dc
Simon Marenkov <simon.marenkov@gmail.com>
2025-04-26 02:04:32 +0300

    Code formatting

--- src/utils/args.ts
+++ src/utils/args.ts
@@ -1,15 +1,15 @@
 export function parseArgs(args: string[]): Record<string, string> {
-    const argMap: Record<string, string> = {};
-    
-    args.forEach((arg, index) => {
-      if (arg.startsWith('-')) {
-        const key = arg.slice(1);
-        const value = args[index + 1];
-        if (value && !value.startsWith('-')) {
-          argMap[key] = value;
-        }
+  const argMap: Record<string, string> = {};
+
+  args.forEach((arg, index) => {
+    if (arg.startsWith('-')) {
+      const key = arg.slice(1);
+      const value = args[index + 1];
+      if (value && !value.startsWith('-')) {
+        argMap[key] = value;
       }
-    });
-  
-    return argMap;
-  } 
+    }
+  });
+
+  return argMap;
+} 
